8f42eb8cb4273ab3f8411ea21c36d8edd3b9cdf0
manel <manelgavalda@iesebre.com>
2016-09-27 21:00:49 +0200

    Added controller and some views

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
+//Clousure o funció anònima, funció que no té nom i per tant no pots cridar-la  a un altre lloc.
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', function() {
+    return view('home');
+});
+
+Route::get('/login', function() {
+    return view('auth.login');
+});
+
+Route::get('/register', function() {
+    return view('auth.register');
 });
