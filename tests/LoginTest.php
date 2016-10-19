b6b9282393a28a8f57642d18f5dfb46672e2a1ba
manelgavalda <manelgavalda@iesebre.com>
2016-10-19 14:57:34 +0200

    Login tests

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -17,4 +17,30 @@ class LoginTest extends TestCase
             ;
     }
 
+    protected function createTestUser() {
+        factory(\App\User::class,1)->create(['password' => bcrypt('123456')])
+        ;
+    }
+
+    public function testLoginPostWithUserOk()
+    {
+        $user = $this->createTestUser();
+        $this->visit('login')
+            ->type($user->email, 'email' )
+            ->type('123456', 'password' )
+            ->press('login')
+            ->seePageIs('/home')
+        ;
+    }
+
+    public function testLoginPostWithUserNotOk()
+    {
+        $this->visit('login')
+            ->type('manel@gmail.com', 'email' )
+            ->type('123456', 'password' )
+            ->press('login')
+            ->seePageIs('/login')
+            //->see('Username not exists')
+        ;
+    }
 }
