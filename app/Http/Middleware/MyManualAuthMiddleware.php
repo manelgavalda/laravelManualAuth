7db1a9c20b5bb1abc95b06ea0beab79668ff1e0c
manel <manelgavalda@iesebre.com>
2016-10-25 20:48:42 +0200

    Middleware refractor

--- app/Http/Middleware/MyManualAuthMiddleware.php
+++ app/Http/Middleware/MyManualAuthMiddleware.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Middleware;
 
+use App\ManualAuth\Guard;
 use Closure;
+//use ManualGuard;
+
+//use ManualGuard;
+
 
 class MyManualAuthMiddleware
 {
+    protected $manualGuard;
+
+    /**
+     * MyManualAuthMiddleware constructor.
+     * @param $manualGuard
+     */
+    public function __construct( \Guard $manualGuard)
+    {
+        $this->manualGuard = $manualGuard;
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -15,6 +31,12 @@ class MyManualAuthMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        //Pecat extrem.
+        //No tornar a explicar el mètode ja que s'enten que és un guard.
+        if ( $this->manualGuard->check()) {
+            return $next($request);
+        }
+        return redirect('login');
+
     }
 }
