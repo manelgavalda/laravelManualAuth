7066ce839dd0e78766f3648419e16b7ab549fa49
manel <manelgavalda@iesebre.com>
2016-11-08 20:58:01 +0100

    Get User By Credentials

---
+++ app/ManualAuth/UserProviders/EloquentUserProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\ManualAuth\UserProviders;
+
+
+use App\User;
+use Hash;
+use Session;
+
+class EloquentUserProvider implements UserProvider
+{
+
+    public function validate(array $credentials)
+    {
+
+
+    //SALTS
+        if ( Hash::check($credentials['password'], $user->password) ) {
+            return true;
+        }
+            Session::flash('errors',collect(["Login incorrecte"]));
+            return false;
+    }
+
+    public function getUserByCredentials(array $credentials)
+    {
+        try {
+        User::where(
+            ["email" => $credentials['email']])->firstOrFail();
+        } catch (\Exception $e) {
+            Session::flash('errors',collect(["Usuari no trobat a la base de dades"]));
+            return false;
+        }
+    }
+}
